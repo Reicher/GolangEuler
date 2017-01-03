6cf2e5f0342a9f360a890b3dcac2e6e3e335101c
robinre <robin.reicher@hiq.se>
2017-01-03 13:56:57 +0100

    A few mor problems solved

--- eulerUtils.go
+++ eulerUtils.go
@@ -2,7 +2,9 @@ package euler
 
 import (
 	"strconv"
+	"math"
 )
+
 
 func GetFibonacci( n int ) []int {
 	fib := []int {1, 2};
@@ -18,7 +20,6 @@ func GetFibonacci( n int ) []int {
 
 func PrimeFactors( n uint64 ) []uint64 {
 	dividers := []uint64{}
-
 	for d := uint64(2); n > d; d++ {
 		if n%d == 0 {
 			dividers = append(dividers, d)
@@ -26,11 +27,9 @@ func PrimeFactors( n uint64 ) []uint64 {
 			break;
 		}
 	}
-
 	if len(dividers) == 0 { // Prime number
 		dividers = append(dividers, n)
 	}
-
 	return dividers
 }
 
@@ -44,20 +43,36 @@ func IsPalindrome( n int ) bool {
 	return true;
 }
 
+func GetPrimesUntil( n int ) []int {
+	primes := []int{2}
+	for i := 3; i < n; i++ {
+		if isPrime(i, primes) {
+			primes = append(primes, i)
+		}
+	}
+	return primes
+}
+
 func GetFirstPrimes( n int ) []int {
 	primes := []int{2}
 	for i := 3; len(primes) < n; i++ {
-		isPrime := true;
-		for _, p := range primes {
-			if i%p == 0 {
-				isPrime = false;
-				break;
-			}
-		}
-		if isPrime {
+		if isPrime(i, primes) {
 			primes = append(primes, i)
 		}
 	}
 
 	return primes
+}
+
+func isPrime( n int, primes []int) bool {
+	maxPossibleFac := int(math.Sqrt(float64(n)))
+	for _, p := range primes{
+		if n%p == 0 {
+			return false
+		}
+		if p > maxPossibleFac {
+			return true
+		}
+	}
+	return true;
 }
